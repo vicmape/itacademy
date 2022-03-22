1bacd88fc398ef696f2e312e630cab8342b357c9
Patricia Gonzalez <waltraute@hotmail.com>
2022-03-22 10:42:44 +0100

    entrega_1_1.js fixes

--- sprint_1/entrega_1_1.js
+++ sprint_1/entrega_1_1.js
@@ -1,6 +1,6 @@
-/******************************************************************************************/
+/******************************************************************************/
 
-// Nivell 1 - Exercici 1
+// N1 E1
 // Crea una funció que mostri per consola el nom d'usuari al invocar-la passant-li el nom com a paràmetre.
 
 function printName(name) {
@@ -9,9 +9,9 @@ function printName(name) {
 
 printName("Victor Machado");
 
-/******************************************************************************************/
+/******************************************************************************/
 
-// Nivell 2 - Exercici 1
+// N2 E1
 // Mostra per consola el nom i cognoms de l'usuari mitjançant template literals, guardant-los en variables i referenciant-les en la impressió del missatge.
 
 function printNameLiterals(name, surname) {
@@ -20,24 +20,24 @@ function printNameLiterals(name, surname) {
 
 printNameLiterals("Victor", "Machado");
 
-/******************************************************************************************/
+/******************************************************************************/
 
-// Nivell 2 - Exercici 2
+// N2 E2
 // Invoca una funció que retorni un valor des de dins d'una template literal.
 
 function getWelcomeMessage() {
-    let message= "We are doing the level 2, exercise 2";
-    return `${message}`;
+    return "Message from function";
 }
 
-console.log(getWelcomeMessage());
+console.log(`This is a template literal getting a message: ${getWelcomeMessage()}`);
 
-/******************************************************************************************/
+/******************************************************************************/
 
-// Nivell 3 - Exercici 1
+// N3 E1
 // Crea una matriu de deu funcions i emplena-la mitjançant un bucle de manera que cada funció compti del 0 al 9 per la consola.
 // Invoca cada funció de l'array iterativament.
 // Haurà de mostrar-se per consola el compte del 0 al 9 deu vegades.
+var arr  = []
 
 function count() {
     for (let i = 0; i < 10; i++) {
@@ -45,17 +45,20 @@ function count() {
     }
 }
 
-var arr = Array(10).fill(count);
+for (let i = 0; i < 10; i++) {
+    arr.push(count)
+}
 
 for(let i = 0; i < arr.length; i++) {
     console.log(`Iteration ${i+1}`);
     arr[i]();
 }
 
-/******************************************************************************************/
+/******************************************************************************/
 
-// Nivell 3 - Exercici 2
+// N3 E2
 // Crea una funció anònima autoinvocable igualada a una variable que mostri per consola el nom de l'usuari rebut com a paràmetre.
 
-const naming = function(name) { console.log(name); };
-var x = naming("Victor");
+var myName = (function(firstName, lastName) {
+    console.log(`This is my name inside an anonymous auto-summoned function: ${firstName} ${lastName}`);
+  })("Victor", "Machado");
