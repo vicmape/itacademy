ea3ee854322eba8bc20b7bfe0cb4a4e51c83bae5
Victor Machado <waltraute@hotmail.com>
2022-03-29 11:02:41 +0200

    entrega_1_6 N2 E1

--- sprint_1/entrega_1_6/__tests__/entregas.test.js
+++ sprint_1/entrega_1_6/__tests__/entregas.test.js
@@ -1,7 +1,7 @@
 
-afterEach(() => {
-    jest.useRealTimers();
-  });
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// NIVELL 1 //////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 //Crea els tests corresponents per verificar el funcionament de l'exercici
 // Async / Await Nivell 1 - Exercici 1
@@ -50,4 +50,29 @@ test('waits 1 second before ending the game', () => {
 
     expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 2000);
+
+    jest.useRealTimers();
+});
+
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// NIVELL 2 //////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// Crea un mock que comprovi les crides al constructor de la classe Persona i al seu mètode decirNombre en l'exercici Classes & Arrow Functions - Nivell 2 Exercici 2
+
+test('Check constructor of class Persona', () => {
+
+    const e_1_2 = require('../../entrega_1_2');
+    jest.mock('../../entrega_1_2');
+
+    // Check persona constructor
+    const persona = new e_1_2.Persona();
+    expect(e_1_2.Persona).toHaveBeenCalledTimes(1);
+
+    // Check dirNom function call
+    const spyDirNom = jest.spyOn(persona, 'dirNom');
+    persona.dirNom();
+    expect(spyDirNom).toHaveBeenCalledTimes(1);
+
 });
