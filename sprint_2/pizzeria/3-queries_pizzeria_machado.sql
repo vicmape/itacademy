62d05d006e53de3d75cd93b82783fc0bad6473bc
Victor Machado <victor.machado.machado@gmail.com>
2022-04-14 14:24:56 +0200

    pizzeria finitto

--- sprint_2/pizzeria/3-queries_pizzeria_machado.sql
+++ sprint_2/pizzeria/3-queries_pizzeria_machado.sql
@@ -2,16 +2,18 @@ USE `pizzeria_machado`;
 
 -- Llista quants productes del tipus 'begudes' s'han venut en una determinada localitat
 SELECT l.nom AS 'localitat' , p.nom AS 'tipus producte', c.id AS 'ID comanda'
-FROM  comandes c
+FROM localitats l
 JOIN botiga b
-	ON c.botiga_id = b.id 
-JOIN localitats l
 	ON b.localitats_id = l.id
+JOIN comandes c
+	ON c.botiga_id = b.id 
+JOIN conte_productes cp
+	ON cp.comandes_id = c.id
 JOIN productes p
-	ON c.productes_id = p.id
+	ON p.id = cp.productes_id
 WHERE l.nom = 'L15' AND p.nom = 'beguda';
     
-    
+
 -- Llista quantes comandes ha efectuat un determinat empleat
 SELECT e.nom AS 'NOM EMPLEAT', c.id AS 'ID COMANDA'
 FROM comandes c
