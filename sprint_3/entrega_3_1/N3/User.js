36c7ea91cd9f524dd36488578d703bf61d930f57
Victor Machado <victor.machado.machado@gmail.com>
2022-04-28 19:12:23 +0200

    users update

--- sprint_3/entrega_3_1/N3/User.js
+++ sprint_3/entrega_3_1/N3/User.js
@@ -5,14 +5,14 @@ class User {
     }
 
     subscribe(topic) {
-        console.log(`${this.name} subscribed to topic ${topic.name}`)
+        console.log(`${this.name} subscribed to ${topic.name}`)
         topic.on(topic.name, arg => {
-            console.log(`${this.name} RECEIVED message "${arg}" from topic ${topic.name}`);
+            console.log(`${this.name} RECEIVED message "${arg}" from ${topic.name}`);
         }) 
     }
 
     sendMessage(topic, message) {
-        console.log(`${this.name} SEND message "${message}" to topic ${topic.name}`);
+        console.log(`${this.name} SEND message "${message}" to ${topic.name}`);
         topic.messageReceived(message);
     }
 }
