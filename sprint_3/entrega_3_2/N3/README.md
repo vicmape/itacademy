c82cffe687860717a4cbbe2a1234409419926646
Victor Machado <victor.machado.perez@gmail.com>
2022-05-02 12:26:22 +0200

    Ex3-2 N3 Publisher-Subscriber

--- sprint_3/entrega_3_2/N3/README.md
+++ sprint_3/entrega_3_2/N3/README.md
@@ -1,5 +1,26 @@
 # Entrega Ex3-2 N3
 
-# Publisher - Subscriber
+## Publisher - Subscriber
 
 Utilitzant RabbitMQ com a element imprescindible crea una queue on una classe Publisher publiqui missatges que siguin llegits per una classe Subscriber. Mostra l'emissió i recepció de cada missatge en consoles diferents
+
+## Sol·lució
+Utilizant RabbitMQ s'han creat dues classes Publisher i Subscriber.
+Aquestes son clases molt simples que implementent el tutorial de hello world the RabbitMQ per publicar/llegir missatges.
+Es suposa que el server de Rabbit MQ esta engegat en el localhost amb el seu port per defecte.
+
+Per provar la sol·lució s'ha d'executar
+
+    npm i
+
+Després s'han d'obrir dos terminal i executar
+
+TERMINAL 1
+
+    node app.js receiver
+
+TERMINAL 2
+
+    node app.js sender
+
+D'aquesta manera, tots els missatges que s'enviin a través del terminal del sender es veuran escrits en l'altre terminal.
--- sprint_3/entrega_3_2/N3/app.js
+++ sprint_3/entrega_3_2/N3/app.js
@@ -1,1 +1,35 @@
-console.log("holi")
+// Import Publisher and Subscriber classes
+const Publisher = require('./Publisher');
+const Subscriber = require('./Subscriber');
+
+// Import readline for command line input
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Function to recursive ask for new message
+function recursiveAsyncReadLine (publisher) {
+  rl.question('Write and press Enter to send: ', function (msg) {
+    publisher.sendMessage(msg);
+    recursiveAsyncReadLine(publisher);
+  });
+};
+
+// Take the argument to see if we are sender or receiver
+var args = process.argv.slice(2);
+
+if (args[0] != null){
+    if (args[0] === 'receiver'){
+        var subscriber = new Subscriber();
+        subscriber.receiveMessages();
+    } else if (args[0] === 'sender') {
+        var publisher = new Publisher();
+        recursiveAsyncReadLine(publisher);
+    } else {
+        console.log('Wrong argument. Options available: [sender|receiver]');
+    }
+} else {
+    console.log('Wrong argument. Options available: [sender|receiver]');
+}
